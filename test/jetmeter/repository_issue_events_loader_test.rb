0eb27ab1011b1597f2bf77167827208e492cc948
Mark Volosiuk <marchi.martius@gmail.com>
2017-05-12 17:00:08 +0300

    Add basic events loader for repo

---
+++ test/jetmeter/repository_issue_events_loader_test.rb
@@ -0,0 +1,17 @@
+require 'minitest/autorun'
+require 'jetmeter/repository_issue_events_loader'
+
+class Jetmeter::RepositoryIssueEventsLoaderTest < Minitest::Test
+  def test_calls_configed_client_for_repository_issue_events
+    mocked_client = Minitest::Mock.new
+    config = OpenStruct.new(
+      client: mocked_client,
+      repository_name: 'marchi-martius/jetmeter'
+    )
+    mocked_client.expect :repository_issue_events, [], ['marchi-martius/jetmeter']
+
+    Jetmeter::RepositoryIssueEventsLoader.new(config).load
+
+    mocked_client.verify
+  end
+end
